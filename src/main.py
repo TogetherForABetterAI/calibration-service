93f6ac75db08d8610764b093183b96a8f464ef14
iinaki <llorensinaki22@gmail.com>
2025-07-07 16:04:55 -0300

    Add full connection between services and MLFlow

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from router import Router
 from lib.connection import setup_rabbitmq
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 middleware = setup_rabbitmq()
 router = Router(middleware)
