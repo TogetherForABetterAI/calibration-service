55132b601abb0cce27ebf9bec03b0be304aa60fb
Iñaki Llorens <llorensinaki22@gmail.com>
2025-07-08 14:51:07 -0300

    feat: Service restore

---
+++ src/router/router.py
@@ -0,0 +1,18 @@
+from controller import Controller
+from lib.config import PROB_OUTPUTS_QUEUE_NAME, DATA_INPUTS_QUEUE_NAME
+
+class Router():
+    def __init__(self, middleware):
+        self._middleware = middleware
+        self._controller = Controller()
+        
+    def start(self):
+        self._middleware.basic_consume(queue_name=PROB_OUTPUTS_QUEUE_NAME, callback_function=self.outputs_callback)
+        self._middleware.basic_consume(queue_name=DATA_INPUTS_QUEUE_NAME, callback_function=self.inputs_callback)
+        self._middleware.start()
+        
+    def outputs_callback(self, ch, method, properties, body):
+        self._controller.process_outputs(ch, method, properties, body)
+        
+    def inputs_callback(self, ch, method, properties, body):
+        self._controller.process_inputs(ch, method, properties, body)
--- src/service/service.py
+++ src/service/service.py
@@ -1,5 +1,73 @@
+import numpy as np
+import pandas as pd
+import mlflow
+import os
+import logging
 
-from random import random
+class MlflowLogger:
+    def __init__(self):
+        project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        artifacts_path = os.path.join(project_root, "artifacts")
+        os.makedirs(artifacts_path, exist_ok=True)
 
-def calculate_error_rate(curr_alpha: float, probabilities: list[float], label: int) -> float:
-    return random()
+        mlflow.set_experiment("Calibration Experiment for MNIST")
+
+        self._artifacts_path = artifacts_path
+        self._batches = {}  
+
+        self._mlflow_run = mlflow.start_run(run_name="EvaluationSession")
+        logging.info("[MLflow] Started global evaluation run")
+
+        self._eof_inputs_received = False
+        self._eof_outputs_received = False
+
+    def store_outputs(self, batch_index: int, probs: np.ndarray, is_last_batch: bool):
+        self._store(batch_index, "probs", probs)
+
+        if is_last_batch:
+            self._eof_outputs_received = True
+            self._try_end_run()
+
+    def store_input_data(self, batch_index: int, inputs: np.ndarray, is_last_batch: bool):
+        self._store(batch_index, "inputs", inputs)
+
+        if is_last_batch:
+            self._eof_inputs_received = True
+            self._try_end_run()
+
+    def _store(self, batch_index: int, kind: str, data: np.ndarray):
+        if batch_index not in self._batches:
+            self._batches[batch_index] = {"inputs": None, "probs": None}
+
+        self._batches[batch_index][kind] = data
+
+        entry = self._batches[batch_index]
+        if entry["inputs"] is not None and entry["probs"] is not None:
+            self.log_single_batch(batch_index, entry["probs"], entry["inputs"])
+            del self._batches[batch_index]
+
+    def log_single_batch(self, batch_index: int, probs: np.ndarray, inputs: np.ndarray):
+        input_flat = inputs.reshape(inputs.shape[0], -1).tolist()
+        probs_list = probs.tolist()
+
+        df = pd.DataFrame({
+            "input": input_flat,
+            "probabilities": probs_list
+        })
+
+        filename = f"batch{batch_index}.parquet"
+        file_path = os.path.join(self._artifacts_path, filename)
+        df.to_parquet(file_path, index=False)
+
+        mlflow.log_artifact(file_path, artifact_path="batches")
+        os.remove(file_path)
+
+        logging.info(f"[MLflow] Logged batch {batch_index} to 'batches/{filename}'")
+
+    def end_run(self):
+        mlflow.end_run()
+        logging.info("[MLflow] Run ended")
+
+    def _try_end_run(self):
+        if self._eof_inputs_received and self._eof_outputs_received:
+            self.end_run()
