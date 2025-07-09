ee574efc16e01d9f1d5ccf71ab94e4da708f7c32
tinchomorilla <mmorilla@fi.uba.ar>
2025-07-09 19:15:04 -0300

    fix: import statement in gRPC service file to use relative import

--- src/pb/new_client_service_pb2_grpc.py
+++ src/pb/new_client_service_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import new_client_service_pb2 as new__client__service__pb2
+from . import new_client_service_pb2 as new__client__service__pb2
 
 GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
