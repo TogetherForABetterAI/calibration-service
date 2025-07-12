bf4c7b1e96fc128598147664a8607a8f38ce0ae1
iinaki <llorensinaki22@gmail.com>
2025-07-11 23:28:55 -0300

    Add plot predictions vs true labels

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -2,6 +2,7 @@ from service import MlflowLogger
 from google.protobuf.message import Message
 from proto import calibration_pb2, dataset_pb2
 import numpy as np
+import logging
 
 class Controller():
     def __init__(self):
@@ -11,8 +12,8 @@ class Controller():
         message = calibration_pb2.Predictions()
         message.ParseFromString(body)
 
-        print(f"Parsed_OUTPUTS: {message}")
-        print(f"Batch index: {message.batch_index}, is_last_batch: {message.eof}")
+        # logging.info(f"Parsed_OUTPUTS: {message}")
+        logging.info(f"[OUTPUTS] Batch index: {message.batch_index}, is_last_batch: {message.eof}")
 
         probs = [list(p.values) for p in message.pred]
         probs_array = np.array(probs, dtype=np.float32)
@@ -25,9 +26,11 @@ class Controller():
 
         
     def process_inputs(self, ch, method, properties, body):    
-        # parseo provisiorio de inputs
         message = dataset_pb2.DataBatch()
         message.ParseFromString(body)
+
+        #logging.info(f"Labels: {message.labels}")
+        logging.info(f"[INPUTS] Batch index: {message.batch_index}, is_last_batch: {message.is_last_batch}")
 
         image_shape = (1, 28, 28)
         image_dtype = np.float32
@@ -41,9 +44,11 @@ class Controller():
             raise ValueError("Tamaño de datos incompatible con imagen")
 
         images = images.reshape((num_images, *image_shape))
+        labels = np.array(message.labels, dtype=np.int32)
 
         self._service.store_input_data(
             batch_index=message.batch_index,
             inputs=images,
-            is_last_batch=message.is_last_batch
+            is_last_batch=message.is_last_batch,
+            labels=labels  
         )
--- src/service/service.py
+++ src/service/service.py
@@ -3,6 +3,9 @@ import pandas as pd
 import mlflow
 import os
 import logging
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 class MlflowLogger:
     def __init__(self):
@@ -21,6 +24,9 @@ class MlflowLogger:
         self._eof_inputs_received = False
         self._eof_outputs_received = False
 
+        self._all_labels = []
+        self._all_probs = []
+
     def store_outputs(self, batch_index: int, probs: np.ndarray, is_last_batch: bool):
         self._store(batch_index, "probs", probs)
 
@@ -28,8 +34,9 @@ class MlflowLogger:
             self._eof_outputs_received = True
             self._try_end_run()
 
-    def store_input_data(self, batch_index: int, inputs: np.ndarray, is_last_batch: bool):
+    def store_input_data(self, batch_index: int, inputs: np.ndarray, is_last_batch: bool, labels: np.ndarray):
         self._store(batch_index, "inputs", inputs)
+        self._store(batch_index, "labels", labels)
 
         if is_last_batch:
             self._eof_inputs_received = True
@@ -37,22 +44,24 @@ class MlflowLogger:
 
     def _store(self, batch_index: int, kind: str, data: np.ndarray):
         if batch_index not in self._batches:
-            self._batches[batch_index] = {"inputs": None, "probs": None}
+            self._batches[batch_index] = {"inputs": None, "probs": None, "labels": None}
 
         self._batches[batch_index][kind] = data
 
         entry = self._batches[batch_index]
-        if entry["inputs"] is not None and entry["probs"] is not None:
-            self.log_single_batch(batch_index, entry["probs"], entry["inputs"])
+        if entry["inputs"] is not None and entry["probs"] is not None and entry["labels"] is not None:
+            self.log_single_batch(batch_index, entry["probs"], entry["inputs"], entry["labels"])
             del self._batches[batch_index]
 
-    def log_single_batch(self, batch_index: int, probs: np.ndarray, inputs: np.ndarray):
+    def log_single_batch(self, batch_index: int, probs: np.ndarray, inputs: np.ndarray, labels: np.ndarray):
         input_flat = inputs.reshape(inputs.shape[0], -1).tolist()
         probs_list = probs.tolist()
+        labels_list = labels.tolist()
 
         df = pd.DataFrame({
             "input": input_flat,
-            "probabilities": probs_list
+            "probabilities": probs_list,
+            "label": labels_list
         })
 
         filename = f"batch{batch_index}.parquet"
@@ -62,9 +71,40 @@ class MlflowLogger:
         mlflow.log_artifact(file_path, artifact_path="batches")
         os.remove(file_path)
 
+        self._all_labels.extend(labels_list)
+        self._all_probs.extend(probs)
+
         logging.info(f"[MLflow] Logged batch {batch_index} to 'batches/{filename}'")
 
+    def _log_prediction_vs_truth(self):
+        y_true = np.array(self._all_labels)
+        probs = np.array(self._all_probs)
+        probs_softmax = self._softmax(probs)
+        y_pred = np.argmax(probs_softmax, axis=1)
+
+        acc = np.mean(y_true == y_pred)
+        logging.info(f"[MLflow] Final accuracy: {acc:.4f}")
+        mlflow.log_metric("accuracy", acc)
+
+        # Matriz de confusión
+        cm = confusion_matrix(y_true, y_pred)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot(cmap='Blues')
+
+        fig_path = os.path.join(self._artifacts_path, "confusion_matrix.png")
+        plt.title("Predictions vs True Labels")
+        plt.savefig(fig_path)
+        plt.close()
+
+        mlflow.log_artifact(fig_path, artifact_path="final_analysis")
+        os.remove(fig_path)
+
+    def _softmax(self, logits):
+        exp_logits = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+        return exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+
     def end_run(self):
+        self._log_prediction_vs_truth()
         mlflow.end_run()
         logging.info("[MLflow] Run ended")
 
