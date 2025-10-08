2b408c50d9a8cd66438ea05a5cc5f48b81addcaf
iinaki <llorensinaki22@gmail.com>
2025-10-08 19:24:22 -0300

    chore: Remove routing key from NewClientRequest:

--- src/pb/new-client-service/new_client_service_pb2.py
+++ src/pb/new-client-service/new_client_service_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18new-client-service.proto\x12\nnew_client\"}\n\x10NewClientRequest\x12\x11\n\tclient_id\x18\x01 \x01(\t\x12\x13\n\x0brouting_key\x18\x02 \x01(\t\x12\x15\n\rinputs_format\x18\x03 \x01(\t\x12\x16\n\x0eoutputs_format\x18\x04 \x01(\t\x12\x12\n\nmodel_type\x18\x05 \x01(\t\"4\n\x11NewClientResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x14\n\x12HealthCheckRequest\"6\n\x13HealthCheckResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t2\xbb\x01\n\x19\x43lientNotificationService\x12N\n\x0fNotifyNewClient\x12\x1c.new_client.NewClientRequest\x1a\x1d.new_client.NewClientResponse\x12N\n\x0bHealthCheck\x12\x1e.new_client.HealthCheckRequest\x1a\x1f.new_client.HealthCheckResponseB6Z4github.com/mlops-eval/data-dispatcher-service/src/pbb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18new-client-service.proto\x12\nnew_client\"h\n\x10NewClientRequest\x12\x11\n\tclient_id\x18\x01 \x01(\t\x12\x15\n\rinputs_format\x18\x02 \x01(\t\x12\x16\n\x0eoutputs_format\x18\x03 \x01(\t\x12\x12\n\nmodel_type\x18\x04 \x01(\t\"4\n\x11NewClientResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x14\n\x12HealthCheckRequest\"6\n\x13HealthCheckResponse\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t2\xbb\x01\n\x19\x43lientNotificationService\x12N\n\x0fNotifyNewClient\x12\x1c.new_client.NewClientRequest\x1a\x1d.new_client.NewClientResponse\x12N\n\x0bHealthCheck\x12\x1e.new_client.HealthCheckRequest\x1a\x1f.new_client.HealthCheckResponseB6Z4github.com/mlops-eval/data-dispatcher-service/src/pbb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,13 +33,13 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'Z4github.com/mlops-eval/data-dispatcher-service/src/pb'
   _globals['_NEWCLIENTREQUEST']._serialized_start=40
-  _globals['_NEWCLIENTREQUEST']._serialized_end=165
-  _globals['_NEWCLIENTRESPONSE']._serialized_start=167
-  _globals['_NEWCLIENTRESPONSE']._serialized_end=219
-  _globals['_HEALTHCHECKREQUEST']._serialized_start=221
-  _globals['_HEALTHCHECKREQUEST']._serialized_end=241
-  _globals['_HEALTHCHECKRESPONSE']._serialized_start=243
-  _globals['_HEALTHCHECKRESPONSE']._serialized_end=297
-  _globals['_CLIENTNOTIFICATIONSERVICE']._serialized_start=300
-  _globals['_CLIENTNOTIFICATIONSERVICE']._serialized_end=487
+  _globals['_NEWCLIENTREQUEST']._serialized_end=144
+  _globals['_NEWCLIENTRESPONSE']._serialized_start=146
+  _globals['_NEWCLIENTRESPONSE']._serialized_end=198
+  _globals['_HEALTHCHECKREQUEST']._serialized_start=200
+  _globals['_HEALTHCHECKREQUEST']._serialized_end=220
+  _globals['_HEALTHCHECKRESPONSE']._serialized_start=222
+  _globals['_HEALTHCHECKRESPONSE']._serialized_end=276
+  _globals['_CLIENTNOTIFICATIONSERVICE']._serialized_start=279
+  _globals['_CLIENTNOTIFICATIONSERVICE']._serialized_end=466
 # @@protoc_insertion_point(module_scope)
