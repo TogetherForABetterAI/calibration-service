92067f7e593ae0a91fc6bf4da3696595ca738f85
tinchomorilla <mmorilla@fi.uba.ar>
2025-10-15 20:23:38 -0300

    move constants to config file

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,5 +1,10 @@
 import os
 
+# RabbitMQ
+DATASET_EXCHANGE = "dataset_exchange"
+REPLIES_EXCHANGE = "replies_exchange"
+CONNECTION_EXCHANGE = "new_connections"
+CONNECTION_QUEUE_NAME = "calibration_service_connections_queue"
 
 class ServerConfig:
     def __init__(self):
